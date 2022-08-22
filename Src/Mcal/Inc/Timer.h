52580327b24d6842049117e2a0d5c33f3af0d093
Pavlyehab <91346837+Pavlyehab@users.noreply.github.com>
2022-08-22 20:49:55 +0200

    Update Timer.h

--- Src/Mcal/Inc/Timer.h
+++ Src/Mcal/Inc/Timer.h
@@ -41,11 +41,11 @@
 /**********************************************************************************************************************
  *  GLOBAL FUNCTION PROTOTYPES
  *********************************************************************************************************************/
-void Gpt_Init(const Gpt_ConfigType* ConfigPtr);
-void Gpt_DisableNotification(Gpt_ChannelType Channel);
-void Gpt_EnableNotification(Gpt_ChannelType Channel);
-void Gpt_StartTimer(Gpt_ChannelType Channel,Gpt_ValueType Value);
-void Gpt_StopTimer(Gpt_ChannelType Channel);
+void Gptm_Init(const Gpt_ConfigType* ConfigPtr);
+void Gptm_DisableNotification(Gpt_ChannelType Channel);
+void Gptm_EnableNotification(Gpt_ChannelType Channel);
+void Gptm_StartTimer(Gpt_ChannelType Channel,Gpt_ValueType Value);
+void Gptm_StopTimer(Gpt_ChannelType Channel);
 Gpt_ValueType Gpt_GetTimeElapsed(Gpt_ChannelType Channel);
 Gpt_ValueType Gpt_GetTimeRemaining(Gpt_ChannelType Channel);
 Std_ReturnType Gpt_GetPredefTimerValue(Gpt_PredefTimerType PredefTimer, uint32* TimeValuePtr);
